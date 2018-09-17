4bcd1e88abe0072531da5c90ecc32e2abd077ed2
Anthony Smith <420061+anthonator@users.noreply.github.com>
2018-09-17 08:02:47 -0400

    Added jason as an optional dependency

--- mix.exs
+++ mix.exs
@@ -20,7 +20,8 @@ defmodule Klaviyo.MixProject do
   defp deps do
     [
       {:dialyxir, "~> 1.0.0-rc", only: [:dev, :test], runtime: false},
-      {:hackney, "~> 1.14", optional: true}
+      {:hackney, "~> 1.14", optional: true},
+      {:jason, "~> 1.1", optional: true}
     ]
   end
 end
