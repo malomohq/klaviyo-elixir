d032cc41a09ba3eea2ac4a977d9fad53c3a8cae1
Anthony Smith <420061+anthonator@users.noreply.github.com>
2018-09-17 09:34:02 -0400

    Added hackney to dialyzer plt

--- mix.exs
+++ mix.exs
@@ -7,7 +7,8 @@ defmodule Klaviyo.MixProject do
       version: "0.1.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      dialyzer: [plt_add_apps: [:hackney]]
     ]
   end
 
