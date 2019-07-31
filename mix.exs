5f57fbddd26882f2164b2f759c2e41732bd1562e
Anthony Smith <420061+anthonator@users.noreply.github.com>
2019-07-31 11:53:11 -0400

    Added package information

--- mix.exs
+++ mix.exs
@@ -7,7 +7,8 @@ defmodule Klaviyo.MixProject do
       version: "2.0.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package()
     ]
   end
 
@@ -23,6 +24,17 @@ defmodule Klaviyo.MixProject do
       { :jason,    "~> 1.1", optional: true },
       { :dialyxir, "~> 1.0.0-rc", only: :dev, runtime: false },
       { :ex_doc,   ">= 0.0.0", only: :dev, runtime: false }
+    ]
+  end
+
+  defp package do
+    [
+      description: "Elixir client for the Klaviyo REST API",
+      maintainers: ["Anthony Smith"],
+      licenses: ["MIT"],
+      links: %{
+        GitHub: "https://github.com/malomohq/klaviyo-elixir"
+      }
     ]
   end
 end
