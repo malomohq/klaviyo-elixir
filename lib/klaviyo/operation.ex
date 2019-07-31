93032f8da7e62dc73724f1710b22b4afbd4be3a6
Anthony Smith <420061+anthonator@users.noreply.github.com>
2019-07-31 16:41:59 -0400

    Perform request in Klaviyo.Operation

--- lib/klaviyo/operation.ex
+++ lib/klaviyo/operation.ex
@@ -1,4 +1,6 @@
 defmodule Klaviyo.Operation do
+  alias Klaviyo.{ Helpers }
+
   @type t ::
           %__MODULE__{
             auth: :private | :public,
@@ -13,4 +15,31 @@ defmodule Klaviyo.Operation do
             method: nil,
             params: %{},
             path: nil
+
+  @spec perform(t, Klaviyo.Config.t()) ::
+        { :ok, Klaviyo.Response.t() } | { :error, Klaviyo.Response.t() | any }
+  def perform(operation, config) do
+    operation = Helpers.Operation.put_auth(operation, config)
+
+    body = Helpers.JSON.encode(operation.params, config)
+    url = Helpers.URL.new(operation, config)
+
+    result =
+      config.http_client.request(
+        operation.method,
+        url,
+        operation.headers,
+        body,
+        config.http_client_opts
+      )
+
+    case result do
+      { :ok, %{ status_code: status_code } = response } when status_code < 400 ->
+        { :ok, Klaviyo.Response.new(response, config) }
+      { :ok, response } ->
+        { :error, Klaviyo.Response.new(response, config) }
+      { :error, _reason } ->
+        result
+    end
+  end
 end
