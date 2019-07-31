16520c115175af8b1a025d80aae8fae76de5aa40
Anthony Smith <420061+anthonator@users.noreply.github.com>
2019-07-31 16:40:10 -0400

    Build response struct from Klaviyo.Response

--- lib/klaviyo/response.ex
+++ lib/klaviyo/response.ex
@@ -1,10 +1,26 @@
 defmodule Klaviyo.Response do
+  alias Klaviyo.{ Helpers }
+
   @type t ::
           %__MODULE__{
-            body: map,
+            body: map | 0 | 1,
             headers: Klaviyo.http_headers_t(),
             status_code: pos_integer
           }
 
   defstruct [:body, :headers, :status_code]
+
+  @spec new(Klaviyo.http_response_t(), Klaviyo.Config.t()) :: t
+  def new(response, config) do
+    body =
+      response
+      |> Map.get(:body)
+      |> Helpers.JSON.decode(config)
+
+    %__MODULE__{
+      body: body,
+      headers: Map.get(response, :headers),
+      status_code: Map.get(response, :status_code)
+    }
+  end
 end
