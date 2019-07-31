14bb221fa164e3c7fe4236b9f394f026893cfc98
Anthony Smith <420061+anthonator@users.noreply.github.com>
2019-07-31 16:36:03 -0400

    Moved the response typespec in the Klaviyo module

--- lib/klaviyo/client.ex
+++ lib/klaviyo/client.ex
@@ -3,18 +3,11 @@ defmodule Klaviyo.Client do
   Behaviour for implementing an HTTP client.
   """
 
-  @type response_t ::
-          %{
-            body: binary,
-            headers: Klaviyo.http_headers_t(),
-            status_code: pos_integer
-          }
-
   @callback request(
               method :: Klaviyo.http_method_t(),
               url :: String.t(),
               headers :: Klaviyo.http_headers_t(),
               body :: binary,
               opts :: any
-            ) :: { :ok, response_t() } | { :error, any }
+            ) :: { :ok, Klaviyo.http_response_t() } | { :error, any }
 end
