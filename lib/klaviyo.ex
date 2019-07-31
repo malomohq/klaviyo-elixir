ad56e5764164ed66a6042bdcaaade86519a56d5c
Anthony Smith <420061+anthonator@users.noreply.github.com>
2019-07-31 16:42:23 -0400

    Added support for the track endpoint

--- lib/klaviyo.ex
+++ lib/klaviyo.ex
@@ -2,4 +2,24 @@ defmodule Klaviyo do
   @type http_headers_t :: [{ String.t(), String.t() }]
 
   @type http_method_t :: :delete | :get | :post | :put
+
+  @type http_response_t ::
+          %{
+            body: binary,
+            headers: http_headers_t,
+            status_code: pos_integer
+          }
+
+  def request(operation, config) do
+    Klaviyo.Operation.perform(operation, Klaviyo.Config.new(config))
+  end
+
+  def track(params) do
+    %Klaviyo.Operation{
+      auth: :public,
+      method: :get,
+      params: params,
+      path: "track"
+    }
+  end
 end
