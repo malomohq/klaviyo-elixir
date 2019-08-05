74ec30bb991c1956387404d96210eac6c83f5b16
Anthony Smith <420061+anthonator@users.noreply.github.com>
2019-08-05 15:59:12 -0400

    Add all public server-side endpoints

--- lib/klaviyo.ex
+++ lib/klaviyo.ex
@@ -10,16 +10,51 @@ defmodule Klaviyo do
             status_code: pos_integer
           }
 
+  @doc """
+  Makes a request to the Klaviyo API.
+  """
+  @spec request(Klaviyo.Operation.t(), Klaviyo.Config.t()) ::
+        { :ok, Klaviyo.Response.t() } | { :error, Klaviyo.Response.t() | any }
   def request(operation, config) do
     Klaviyo.Operation.perform(operation, Klaviyo.Config.new(config))
   end
 
+  @doc """
+  Track properties about an individual without tracking an associated event.
+  """
+  @spec identify(map) :: Klaviyo.Operation.t()
+  def identify(params) do
+    %Klaviyo.Operation{
+      auth: :public,
+      method: :get,
+      params: params,
+      path: "identify"
+    }
+  end
+
+  @doc """
+  Track when someone takes an action or does something.
+  """
+  @spec track(map) :: Klaviyo.Operation.t()
   def track(params) do
     %Klaviyo.Operation{
       auth: :public,
       method: :get,
       params: params,
       path: "track"
+    }
+  end
+
+  @doc """
+  Tracks the first occurance of when someone takes an action or does something.
+  """
+  @spec track_once(map) :: Klaviyo.Operation.t()
+  def track_once(params) do
+    %Klaviyo.Operation{
+      auth: :public,
+      method: :get,
+      params: params,
+      path: "track-once"
     }
   end
 end
