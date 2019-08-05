64e9dbfe6ca3e3ad00450727019f7497c2ef3d79
Anthony Smith <420061+anthonator@users.noreply.github.com>
2019-08-05 17:37:32 -0400

    Remaining list endpoints

--- lib/klaviyo/list.ex
+++ lib/klaviyo/list.ex
@@ -1,5 +1,36 @@
 defmodule Klaviyo.List do
   @doc """
+  Add people to a list without changing their subscription or suppression
+  status.
+  """
+  @spec add_as_member(String.t(), map) :: Klaviyo.Operation.t()
+  def add_as_member(list_id, params) do
+    %Klaviyo.Operation{
+      auth: :private,
+      method: :post,
+      params: params,
+      path: "v2/list/#{list_id}/members"
+    }
+  end
+
+  @doc """
+  Add people to a list in addition to modifying their membership and updating
+  their suppressions.
+
+  People will be single or double opted into the specified list in accordance
+  with that list's settings.
+  """
+  @spec add_as_subscriber(String.t(), map) :: Klaviyo.Operation.t()
+  def add_as_subscriber(list_id, params) do
+    %Klaviyo.Operation{
+      auth: :private,
+      method: :post,
+      params: params,
+      path: "v2/list/#{list_id}/subscribe"
+    }
+  end
+
+  @doc """
   Returns an array of lists.
   """
   @spec all :: Klaviyo.Operation.t()
@@ -8,6 +39,33 @@ defmodule Klaviyo.List do
       auth: :private,
       method: :get,
       path: "v2/lists"
+    }
+  end
+
+  @doc """
+  Get all emails that have been excluded from a list along with the reason and
+  time they were excluded.
+  """
+  @spec all_exclusions(String.t(), map) :: Klaviyo.Operation.t()
+  def all_exclusions(list_id, params \\ %{}) do
+    %Klaviyo.Operation{
+      auth: :private,
+      method: :get,
+      params: params,
+      path: "v2/list/#{list_id}/exclusions/all",
+    }
+  end
+
+  @doc """
+  Get all emails in a given list or segment.
+  """
+  @spec all_group_members(String.t(), map) :: Klaviyo.Operation.t()
+  def all_group_members(list_id_or_segment_id, params \\ %{}) do
+    %Klaviyo.Operation{
+      auth: :private,
+      method: :get,
+      params: params,
+      path: "v2/group/#{list_id_or_segment_id}/members/all"
     }
   end
 
@@ -45,6 +103,57 @@ defmodule Klaviyo.List do
       auth: :private,
       method: :get,
       path: "v2/list/#{list_id}"
+    }
+  end
+
+  @doc """
+  Get people that are on a list.
+  """
+  @spec get_memberships(String.t(), map) :: Klaviyo.Operation.t()
+  def get_memberships(list_id, params) do
+    %Klaviyo.Operation{
+      auth: :private,
+      method: :get,
+      params: params,
+      path: "v2/list/#{list_id}/members"
+    }
+  end
+
+  @doc """
+  Get people that are on a list and not suppressed.
+  """
+  @spec get_subscriptions(String.t(), map) :: Klaviyo.Operation.t()
+  def get_subscriptions(list_id, params) do
+    %Klaviyo.Operation{
+      auth: :private,
+      method: :get,
+      params: params,
+      path: "v2/list/#{list_id}/subscribe"
+    }
+  end
+
+  @doc """
+  Remove members from a list
+  """
+  def remove_as_member(list_id, params) do
+    %Klaviyo.Operation{
+      auth: :private,
+      method: :delete,
+      params: params,
+      path: "v2/list/#{list_id}/members"
+    }
+  end
+
+  @doc """
+  Remove subscribers from a list.
+  """
+  @spec remove_as_subscriber(String.t(), map) :: Klaviyo.Operation.t()
+  def remove_as_subscriber(list_id, params) do
+    %Klaviyo.Operation{
+      auth: :private,
+      method: :delete,
+      params: params,
+      path: "v2/list/#{list_id}/subscribe"
     }
   end
 
--- test/klaviyo/list_test.exs
+++ test/klaviyo/list_test.exs
@@ -1,12 +1,72 @@
 defmodule Klaviyo.ListTest do
   use ExUnit.Case, async: true
 
+  test "add_as_member/2" do
+    id = "1"
+
+    p = %{ a: "b" }
+
+    path = "v2/list/#{id}/members"
+
+    %Klaviyo.Operation{
+      auth: :private,
+      method: :post,
+      params: ^p,
+      path: ^path
+    } = Klaviyo.List.add_as_member(id, p)
+  end
+
+  test "add_as_subscriber/2" do
+    id = "1"
+
+    p = %{ a: "b" }
+
+    path = "v2/list/#{id}/subscribe"
+
+    %Klaviyo.Operation{
+      auth: :private,
+      method: :post,
+      params: ^p,
+      path: ^path
+    } = Klaviyo.List.add_as_subscriber(id, p)
+  end
+
   test "all/0" do
     %Klaviyo.Operation{
       auth: :private,
       method: :get,
       path: "v2/lists"
     } = Klaviyo.List.all()
+  end
+
+  test "all_exclusions/2" do
+    id = "1"
+
+    p = %{ a: "b" }
+
+    path = "v2/list/#{id}/exclusions/all"
+
+    %Klaviyo.Operation{
+      auth: :private,
+      method: :get,
+      params: ^p,
+      path: ^path
+    } = Klaviyo.List.all_exclusions(id, p)
+  end
+
+  test "all_group_members/2" do
+    id = "1"
+
+    p = %{ a: "b" }
+
+    path = "v2/group/#{id}/members/all"
+
+    %Klaviyo.Operation{
+      auth: :private,
+      method: :get,
+      params: ^p,
+      path: ^path
+    } = Klaviyo.List.all_group_members(id, p)
   end
 
   test "create/1" do
@@ -42,6 +102,66 @@ defmodule Klaviyo.ListTest do
       method: :get,
       path: ^path
     } = Klaviyo.List.get(id)
+  end
+
+  test "get_memberships/2" do
+    id = "1"
+
+    p = %{ a: "b" }
+
+    path = "v2/list/#{id}/members"
+
+    %Klaviyo.Operation{
+      auth: :private,
+      method: :get,
+      params: ^p,
+      path: ^path
+    } = Klaviyo.List.get_memberships(id, p)
+  end
+
+  test "get_subscriptions/2" do
+    id = "1"
+
+    p = %{ a: "b" }
+
+    path = "v2/list/#{id}/subscribe"
+
+    %Klaviyo.Operation{
+      auth: :private,
+      method: :get,
+      params: ^p,
+      path: ^path
+    } = Klaviyo.List.get_subscriptions(id, p)
+  end
+
+  test "remove_as_member/2" do
+    id = "1"
+
+    p = %{ a: "b" }
+
+    path = "v2/list/#{id}/members"
+
+    %Klaviyo.Operation{
+      auth: :private,
+      method: :delete,
+      params: ^p,
+      path: ^path
+    } = Klaviyo.List.remove_as_member(id, p)
+  end
+
+  test "remove_as_subscriber/2" do
+    id = "1"
+
+    p = %{ a: "b" }
+
+    path = "v2/list/#{id}/subscribe"
+
+    %Klaviyo.Operation{
+      auth: :private,
+      method: :delete,
+      params: ^p,
+      path: ^path
+    } = Klaviyo.List.remove_as_subscriber(id, p)
   end
 
   test "update/2" do
