d0b41f8814c091a5edb3f396aa899f9e7b8085d5
Anthony Smith <420061+anthonator@users.noreply.github.com>
2019-08-05 15:59:57 -0400

    Support metric endpoints

---
+++ test/klaviyo/metric_test.exs
@@ -0,0 +1,55 @@
+defmodule Klaviyo.MetricTest do
+  use ExUnit.Case, async: true
+
+  test "all/1" do
+    p = %{ a: "b" }
+
+    assert %Klaviyo.Operation{
+      auth: :private,
+      method: :get,
+      params: ^p,
+      path: "v1/metrics"
+    } = Klaviyo.Metric.all(p)
+  end
+
+  test "export/2" do
+    id = "1"
+
+    p = %{ a: "b" }
+
+    path = "v1/metric/#{id}/export"
+
+    assert %Klaviyo.Operation{
+      auth: :private,
+      method: :get,
+      params: ^p,
+      path: ^path
+    } = Klaviyo.Metric.export(id, p)
+  end
+
+  test "timeline/1" do
+    p = %{ a: "b" }
+
+    assert %Klaviyo.Operation{
+      auth: :private,
+      method: :get,
+      params: ^p,
+      path: "v1/metrics/timeline"
+    } = Klaviyo.Metric.timeline(p)
+  end
+
+  test "timeline/2" do
+    id = "1"
+
+    p = %{ a: "b" }
+
+    path = "v1/metric/#{id}/timeline"
+
+    assert %Klaviyo.Operation{
+      auth: :private,
+      method: :get,
+      params: ^p,
+      path: ^path
+    } = Klaviyo.Metric.timeline(id, p)
+  end
+end
