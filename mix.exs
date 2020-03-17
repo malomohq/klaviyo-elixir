10fea9a76c3b79618cf7b542f56850ad47cda058
Anthony Smith <420061+anthonator@users.noreply.github.com>
2020-03-17 09:43:39 -0400

    Support Elixir 1.8

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Klaviyo.MixProject do
     [
       app: :klaviyo,
       version: "2.1.0",
-      elixir: "~> 1.9",
+      elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       dialyzer: [plt_add_apps: [:hackney]],
