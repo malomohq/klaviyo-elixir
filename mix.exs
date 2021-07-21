44a82594f83b083410e9b90bef25b1de4c0d4ccc
Anthony Smith <420061+anthonator@users.noreply.github.com>
2021-07-21 13:39:52 -0400

    Bumped version to 2.2.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Klaviyo.MixProject do
   def project do
     [
       app: :klaviyo,
-      version: "2.1.1",
+      version: "2.2.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
