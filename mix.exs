68196dbc91ac52cba51273d8ddeed0ff0dcf3f16
Anthony Smith <420061+anthonator@users.noreply.github.com>
2024-05-20 18:01:56 -0400

    chore: bumped version to 3.1.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Klaviyo.MixProject do
   def project do
     [
       app: :klaviyo,
-      version: "3.0.0",
+      version: "3.1.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
